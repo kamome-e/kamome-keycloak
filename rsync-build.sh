9675423547eaf1b0a1674d1d0c0987c5630fdb35
Taisei Morigami <taisei.morigami.2014@alit.jp>
2019-07-02 13:09:53 +0900

    KEYCLOAK_KAMOME_SSO-43 Keycloak開発環境の構築 kamome-keycloak 用に対応

--- rsync-build.sh
+++ rsync-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -av --exclude '*/.git' --exclude '*/target' --exclude '*/dependency-reduced-pom.xml' --exclude '*/bin' --delete /vagrant/kamome-keycloak/ /usr/src/keycloak/
-cd /usr/src/keycloak
+rsync -av --exclude '*/.git' --exclude '*/target' --exclude '*/dependency-reduced-pom.xml' --exclude '/kamome-keycloak/bin' --delete /vagrant/kamome-keycloak /usr/src/
+cd /usr/src/kamome-keycloak
 
 export MAVEN_OPTS="-Xmx1024m -XX:MaxMetaspaceSize=512m"
 
@@ -9,4 +9,4 @@ mvn clean install -Pdistribution -DskipTests=true
 
 cd /vagrant
 
-cp -p /usr/src/keycloak/distribution/server-dist/target/keycloak-4.8.3.Final.tar.gz /vagrant
+cp -p /usr/src/kamome-keycloak/distribution/server-dist/target/keycloak-4.8.3.Final.tar.gz /vagrant
